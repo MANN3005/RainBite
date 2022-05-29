2d88e86c48e4e68c7243eb6f17ad3f097954c693
Mann Patel <mannpatel7555@gmail.com>
2022-05-29 15:39:59 +0530

    Menu form

--- application/views/admin/Menu.php
+++ application/views/admin/Menu.php
@@ -1,12 +1,14 @@
- /box_general
-            <div class="box_general padding_bottom">
+ <!-- /box_general -->
+ <form method="post"  action="<?php echo base_url('Admin/Menu');?>" autocomplete = "off">
+     <div class="box_general padding_bottom">
                 <div class="header_box version_2">
                     <h2><i class="fa fa-list"></i>Add item to Menu List</h2>
                 </div>
-                <div class="wrapper_indent">
+    
+            <div class="wrapper_indent">
                     <div class="form-group">
                         <label>Menu Category</label>
-                        <input type="text" name="menu_category" class="form-control" placeholder="EX. Starters">
+                        <input type="text" name="menu_category"  value="<?php echo set_value('menu_category');?>"class="form-control" placeholder="EX. Starters">
                     </div>
                     <div class="menu-item-section clearfix">
                         <h4>Menu item #1</h4>
@@ -16,28 +18,28 @@
                     <div class="strip_menu_items">
                         <div class="row">
                             <div class="col-xl-3">
-                                <form action="/file-upload" class="dropzone"></form>
                             </div>
                             <div class="col-xl-9">
                                 <div class="row">
                                     <div class="col-md-8">
                                         <div class="form-group">
                                             <label>Title</label>
-                                            <input type="text" name="menu_item_title" class="form-control">
+                                            <input type="text" name="item_name"  value="<?php echo set_value('menu_category');?>" class="form-control">
                                         </div>
                                     </div>
                                     <div class="col-md-4">
                                         <div class="form-group">
                                             <label>Price</label>
-                                            <input type="text" name="menu_item_title_price" class="form-control">
+                                            <input type="text" name="price" value="<?php echo set_value('price');?>" class="form-control">
                                         </div>
                                     </div>
                                 </div>
-                                <div class="form-group">
+                                <p><button type="submit" value="Save" class="btn_1 medium gray">Save</button></p>
+                                <!-- <div class="form-group">
                                     <label>Short description</label>
                                     <input type="text" name="menu_item_description" class="form-control">
-                                </div>
-                                <div class="form-group">
+                                </div> -->
+                                <!-- <div class="form-group">
                                     <label>Item options</label>
                                     <div class="item_opt_wrapper">
                                         <div class="row">
@@ -64,10 +66,10 @@
                                                 </div>
                                             </div>
                                         </div> -->
-                                        /row-->
-                                    </div>
+                                        <!-- /row--> 
+                                    <!-- </div> --> 
                                      <!-- /item_opt_wrapper -->
-                                    <div class="item_opt_wrapper" style="background-color: #fff;">
+                                    <!-- <div class="item_opt_wrapper" style="background-color: #fff;">
                                         <div class="row">
                                             <div class="col-lg-3 col-md-4">
                                                 <div class="form-group">
@@ -91,14 +93,14 @@
                                                     </label>
                                                 </div>
                                             </div>
-                                        </div> --> -->
-                                        <!-- /row-->
-                                    </div>
+                                        </div> --> 
+                                        <!-- /row --> 
+                                    <!-- </div> -->
                                     <!-- /item_opt_wrapper-->
-                                 </div> -->
+                                 <!-- </div> --> 
                                 <!-- End form-group -->
-                                <div class="form-group">
-                                    <label>Item ingredients</label>
+                                <!-- <div class="form-group"> -->
+                                    <!-- <label>Item ingredients</label>
                                     <div class="item_opt_wrapper">
                                         <div class="row">
                                             <div class="col-lg-3 col-md-4">
@@ -124,10 +126,10 @@
                                                 </div>
                                             </div>
                                         </div> -->
-                                        <!-- /row-->
-                                    </div> -->
+                                           <!-- /row-->
+                                    <!-- </div> --> 
                                     <!-- /item_opt_wrapper-->
-                                     <div class="item_opt_wrapper" style="background-color: #fff;">
+                                     <!-- <div class="item_opt_wrapper" style="background-color: #fff;">
                                         <div class="row">
                                             <div class="col-lg-3 col-md-4">
                                                 <div class="form-group">
@@ -150,19 +152,19 @@
                                                         <span class="checkmark"></span>
                                                     </label>
                                                 </div>
-                                            </div>
-                                        </div> -->
+                                            </div> -->
+                                        <!-- </div> --> 
                                         <!-- /row-->
-                                    </div> -->
+                                    <!-- </div> --> 
                                     <!-- End form-group -->
-                                </div> -->
-                            </div> -->
+                                <!-- </div> --> 
+                            </div> 
                             <!-- End row -->
-                        </div> -->
+                        </div>
                         <!-- End strip_menu_items -->
-                    </div> -->
+                    </div> 
                     <!-- /box_general-->
-                </div> -->
+                </div> 
                 <!-- /.container-fluid-->
-            </div> -->
+            </div>
             <!-- /.container-wrapper
